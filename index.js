5fe27cd75ec0e2bbe8c883c704e922c384d6d366
Lokesh1933 <167898603+Lokesh1933@users.noreply.github.com>
2025-05-16 20:02:54 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Set up EJS and public folder here
+
+// Add bodyParser
+
+// GET / route to render index.ejs
+
+// POST /shorten route to use Axios & Bitly API
+
+// Start the server

da17b48fbd2ac6e8e2f4f61cf4b8f74123e94d55
Lokesh1933 <167898603+Lokesh1933@users.noreply.github.com>
2025-05-16 21:31:00 +0530

    hello

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000
 
+
 // Set up EJS and public folder here
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine','ejs')
@@ -14,14 +15,26 @@ app.set('views',path.join(__dirname,'views'))
 app.use(bodyParser.urlencoded({extended: true}))
 
 // Add bodyParser
-
+const config = {
+     headers: {
+        'Authorization': `Bearer ${bearerToken}`,
+     }
+}
 // GET / route to render index.ejs
-app.get("/",async (req,res) => {
-    res,render('index')
+app.get("/",(req,res) => {
+    res.render('index')
 })
 
 // POST /shorten route to use Axios & Bitly API
-app.post()
+app.post("/shorten",async (req,res) => {
+    const bearerToken = process.env.BITLY_TOKEN
+    try {
+        const result = await axios.post(`https://api-ssl.bitly.com/v4/shorten`,config)
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+})
 
 // Start the server
 app.listen(port, () => {
